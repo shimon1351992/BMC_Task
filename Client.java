5974f01c850ee2c37ac4476bce2a4cfc4a5a9140
shimon1351992 <73971771+shimon1351992@users.noreply.github.com>
2022-07-19 18:18:07 +0300

    Update Client.java

--- Client.java
+++ Client.java
@@ -6,9 +6,9 @@ public class Client  extends Thread{
 
 
     public static void main(String[] args) throws IOException {
-        Socket s = new Socket(args[0],6666);
+        Socket s = new Socket(args[1],6666);
         DataOutputStream dout = new DataOutputStream(s.getOutputStream());
-        dout.writeUTF(args[1]);
+        dout.writeUTF(args[2]);
         dout.flush();
         dout.close();
         s.close();
