906fb1c11f8cbb2d1903f60345900cd516babdf4
shimon1351992 <73971771+shimon1351992@users.noreply.github.com>
2022-07-24 11:37:06 +0300

    wuth args[0] and args[1]

--- Client/Client.java
+++ Client/Client.java
@@ -6,11 +6,11 @@ import java.net.Socket;
 public class Client {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         //args[0] hold the ip from jenkinsfile and create socket
-        Socket s = new Socket("192.168.56.1",6666);
+        Socket s = new Socket(args[0],6666);
 
         // args[1] hold the path for directory from jenkinsfile and  send to server
         DataOutputStream dout = new DataOutputStream(s.getOutputStream());
-        dout.writeUTF("C:/WINDOWS");
+        dout.writeUTF(args[1]);
 
         // get the message from server
         ObjectInputStream objectInputStream = new ObjectInputStream(s.getInputStream());
